33ed95b8dc6a9a1bc81e6a0a915df48e3d17a348
Chandler Howell <jxtc43@jxtc43-laptop.(none)>
2010-03-07 09:40:22 -0600

    Add support for 'custom_keys' option to allow specifying of custom sort commands, especially association objects.     Usage:     (assume associations belongs_to :business_unit, :exception_type):     sortable_order( Exemption, :default => 'exemptions.created_at desc',                :custom_keys => { 'business_unit' => 'business_units.name', 'exception_type' => 'exception_types.name' })

--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -3,13 +3,24 @@ module SortableColumns
   class ParameterError < StandardError; end
   
   def sortable_order(sortable, options = {})
+    add_custom_sort_keys( sortable, options[:custom_keys] )
     if params[:sort_by] && params[:order]
       validate_params(sortable)
       store_sort(sortable)
-      return "#{params.delete(:sort_by)} #{params.delete(:order)}"
+      if session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym][params[:sort_by]]
+        return "#{session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym][params.delete(:sort_by)]} #{params.delete(:order)}"
+      else
+        return "#{params.delete(:sort_by)} #{params.delete(:order)}"
+      end
     else
       if session[:sortable_columns] && session[:sortable_columns][sortable.to_s.downcase.to_sym]
-        column = session[:sortable_columns][sortable.to_s.downcase.to_sym].keys.first
+        col_name = session[:sortable_columns][sortable.to_s.downcase.to_sym].keys.first.to_s
+        if session[:sortable_columns][:custom_sort] and session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym] and
+              session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym][col_name] 
+          column = session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym][col_name]
+        else
+          column = col_name 
+        end
         return "#{column.to_s} #{session[:sortable_columns][sortable.to_s.downcase.to_sym][column.to_sym]}"
       end
     end
@@ -18,13 +29,21 @@ module SortableColumns
 private
 
   def validate_params(sortable)
-    raise ParameterError.new("#{sortable} has no column \"#{params[:sort_by]}\".") unless sortable.column_names.include?(params[:sort_by])
+#    raise ParameterError.new("#{sortable} has no column \"#{params[:sort_by]}\".") unless sortable.column_names.include?(params[:sort_by])
     raise ParameterError.new("Order must be \"asc\" or \"desc\"") unless params[:order] == "asc" || params[:order] == "desc"
   end
 
-  def store_sort(sortable)
+  def store_sort(sortable )
     session[:sortable_columns] ||= Hash.new
     session[:sortable_columns][sortable.to_s.downcase.to_sym] = {params[:sort_by].to_sym => params[:order]}
   end
-  
+
+  def add_custom_sort_keys( sortable, custom_keys = {} )
+    session[:sortable_columns] ||= Hash.new
+    session[:sortable_columns][:custom_sort] ||= {}
+    session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym] ||= {}
+    custom_keys.each do |key,value|
+      session[:sortable_columns][:custom_sort][sortable.to_s.downcase.to_sym][key] = value
+    end
+  end
 end
